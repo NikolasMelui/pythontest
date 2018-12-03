ed12b1e4a1182fe87269d0ff748627217c3330c0
nikolasmelui <slak@samaradom.ru>
2018-12-04 02:36:07 +0400

    Add rename to the no_data column

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
 
-content = pd.read_csv('revenue-profit.csv', sep=';')
+content = pd.read_csv('revenue-profit.csv',
+                      sep=';').rename(columns={'no_data': 'Year'})
 print(content)
