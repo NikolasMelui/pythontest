b2745a2b45f85a1be42bf5b679e12d67abc8e2f4
nikolasmelui <slak@samaradom.ru>
2018-12-04 02:42:01 +0400

    Add matplotlib (and it's working!)

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 content = pd.read_csv('revenue-profit.csv',
                       sep=';').rename(columns={'no_data': 'Year'})
 print(content)
+
+plt.plot(content.Year, content.Revenue)
+plt.xlabel('Year')
+plt.ylabel('Revenue')
+plt.show()
