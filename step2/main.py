2466de3eed64e60a072a0afee01925dac9d6b61e
nikolasmelui <slak@samaradom.ru>
2018-12-04 11:24:44 +0400

    Add new functional to get 'names' from the json data

---
+++ step2/main.py
@@ -0,0 +1,15 @@
+import json
+
+
+def get_user_names(path):
+    with open(path) as access_json:
+        read_content = json.load(access_json)
+        question_content = read_content['results']
+        for question_data in question_content:
+            question_replies = question_data['replies']
+            for replies_data in question_replies:
+                user_name = replies_data['user']['display_name']
+                print(user_name)
+
+
+get_user_names('./source-data.json')
